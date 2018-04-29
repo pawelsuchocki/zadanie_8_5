cf81b7307544bcf820c42f1b4fddfee00c1d7767
Pawel Suchocki <pawel.suchocki212@gmail.com>
2018-04-29 20:29:20 +0200

    add script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var a = 2,
+	h = 4,
+	triangleArea = 'area';
+
+triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
